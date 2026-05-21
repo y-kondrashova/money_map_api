dae6655b14d306ef371ba1eade610078b82acab1
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-21 12:47:44 +0300

    Add pytest for user profile

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,23 @@
 import pytest
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+
+User = get_user_model()
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def user():
+    return User.objects.create_user(
+        email="test@example.com", password="strongpassword123"
+    )
+
+
+@pytest.fixture
+def authenticated_client(api_client, user):
+    api_client.force_authenticate(user=user)
+    return api_client
