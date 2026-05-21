15ad294493eb684b0afd3015726d8f4afc4b8488
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-21 17:48:47 +0300

    Add viewset for managing categories

---
+++ apps/category/views.py
@@ -0,0 +1,20 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user, is_active=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
