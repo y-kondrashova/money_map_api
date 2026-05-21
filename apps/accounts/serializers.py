dc2f1a4ef83db55b6a79f1b114b888804e27ca03
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-21 09:47:32 +0300

    Add serializers for user registration

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "password",
+            "password_confirm",
+            "created_at",
+        ]
+        read_only_fields = ["id", "created_at"]
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_confirm"]:
+            raise serializers.ValidationError(
+                {"password_confirm": "Passwords do not match."}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+
+        return user
+
+
+class RegisterResponseSerializer(serializers.ModelSerializer):
+    access = serializers.SerializerMethodField()
+    refresh = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "created_at",
+            "access",
+            "refresh",
+        ]
+
+    @staticmethod
+    def get_access(user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+
+    @staticmethod
+    def get_refresh(user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh)
