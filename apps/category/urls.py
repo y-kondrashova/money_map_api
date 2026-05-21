ee44e01ab2cd4f9e93888b00bcb14a3baf81bfa0
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-21 17:49:27 +0300

    Add url patterns for category app

---
+++ apps/category/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from .views import CategoryViewSet
+
+router = DefaultRouter()
+router.register("", CategoryViewSet, basename="category")
+
+urlpatterns = router.urls
--- config/urls.py
+++ config/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("api/auth/", include("apps.accounts.urls")),
     path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/category/", include("apps.category.urls")),
 ]
