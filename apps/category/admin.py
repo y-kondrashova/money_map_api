91d8bf95a94863b0003c543a92da5fa2db30d9c7
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-22 09:43:10 +0300

    Add category model to admin panel

---
+++ apps/category/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ("name",)
+    list_display = (
+        "id",
+        "name",
+        "category_type",
+        "user",
+        "is_active",
+        "created_at",
+        "updated_at",
+    )
+    list_link = ("id", "name")
+    list_editable = ("is_active",)
+    list_filter = ("category_type", "is_active", "created_at")
+    search_fields = ("name", "user__email", "user__first_name", "user__last_name")
+    autocomplete_fields = ("user",)
+    readonly_fields = ("id", "created_at", "updated_at")
+    date_hierarchy = "created_at"
+    list_per_page = 25
+    list_select_related = ("user",)
