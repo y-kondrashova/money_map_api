5f8cb481193bd8096674ffdfd2a996cde9451b8d
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-22 12:25:07 +0300

    Add validation for unique category

--- apps/category/serializers.py
+++ apps/category/serializers.py
@@ -14,4 +14,35 @@ class CategorySerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
-        read_only_fields = ["id", "is_active", "created_at", "updated_at"]
+        read_only_fields = [
+            "id",
+            "is_active",
+            "created_at",
+            "updated_at",
+        ]
+
+    def validate(self, attrs):
+        request = self.context.get("request")
+        user = request.user
+
+        name = attrs.get("name", getattr(self.instance, "name", None))
+        category_type = attrs.get(
+            "category_type",
+            getattr(self.instance, "category_type", None),
+        )
+
+        category_exists = Category.objects.filter(
+            user=user,
+            name=name,
+            category_type=category_type,
+        )
+
+        if self.instance:
+            category_exists = category_exists.exclude(pk=self.instance.pk)
+
+        if category_exists.exists():
+            raise serializers.ValidationError(
+                {"name": "Category with this name and type already exists."}
+            )
+
+        return attrs
