36462d093b34908ff0bb0b35e6b5e04ced550d03
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2026-05-28 18:17:16 +0300

    Add restore and deleted actions for wallet views

--- apps/wallets/views.py
+++ apps/wallets/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Wallet
 from .serializers import WalletSerializer
@@ -10,7 +12,11 @@ class WalletViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Wallet.objects.filter(user=self.request.user, is_active=True)
+        queryset = Wallet.objects.filter(user=self.request.user)
+
+        if self.action == "list":
+            return queryset.filter(is_active=True)
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -18,3 +24,17 @@ class WalletViewSet(viewsets.ModelViewSet):
     def perform_destroy(self, instance):
         instance.is_active = False
         instance.save()
+
+    @action(detail=True, methods=["post"])
+    def restore(self, request, pk=None):
+        wallet = self.get_object()
+        wallet.is_active = True
+        wallet.save()
+        serializer = self.get_serializer(wallet)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["get"])
+    def deleted(self, request):
+        wallets = Wallet.objects.filter(user=self.request.user, is_active=False)
+        serializer = self.get_serializer(wallets, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
